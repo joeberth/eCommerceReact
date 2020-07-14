5f1919f4c347b465a2a52344ccd4c98d5544c0a1
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2020-07-14 05:11:43 -0300

    Add itens front to cart

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
