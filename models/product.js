aa6f1d5f40ce68ffacb5ae9e999803e995b88ff6
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2020-07-14 19:53:04 -0300

    change product lenght

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
             type: String,
             trim: true,
             required: true,
-            maxlength: 32
+            maxlength: 60
         },
         description: {
             type: String,
